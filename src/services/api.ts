5bb122143c4ac74161303d17a730351af832c058
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-08-07 13:10:10 -0300

    Adicionando URL de produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.7:3333'
+    baseURL: 'https://proffy-nlw.herokuapp.com' // url produção
+    //baseURL: 'http://192.168.0.7:3333' // url de teste
 });
 
 export default api;

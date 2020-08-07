353cfb7375e8d1ca82c74d3ad1443b3ea8fb1dfc
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-08-07 12:18:45 -0300

    Integrando listagem de proffys com backend com linking whatsapp

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
-import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { View, Image, Text, Linking } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
@@ -8,29 +8,48 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles'
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string,
+    bio: string,
+    cost: number,
+    id: number,
+    name: string,
+    subject: string,
+    whatsapp: string  
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ( { teacher } ) => {
+    
+    function handleLinkToWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+    }
+    
     return (
         <View style={styles.container}>
             <View style={styles.profile}> 
                 <Image 
                     style={styles.avatar}
-                    source={{ uri: 'https://github.com/MarceloCosta02.png'  }}
+                    source={{ uri: teacher.avatar }}
                 />
 
                 <View style={styles.profileInfo}>
-                    <Text style={styles.name}>Marcelo de Lucca</Text>
-                    <Text style={styles.subject}>Programação</Text>
+                    <Text style={styles.name}>{teacher.name}</Text>
+                    <Text style={styles.subject}>{teacher.subject}</Text>
                 </View>
             </View>
 
             <Text style={styles.bio}>
-                Analista de Sistemas com experiência na plataforma .Net da Microsoft, e também nas Linguagens Java, C, Html, CSS, Javascript, JQuery, React Native e Bancos SQL/NoSQL. 
+                {teacher.bio}
             </Text>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
                     Preço/hora {'   '}
-                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                    <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
                 </Text>
 
                 <View style={styles.buttonsContainer}>
@@ -39,7 +58,7 @@ function TeacherItem() {
                         <Image source={unFavoriteIcon}/>
                     </RectButton>
 
-                    <RectButton style={styles.contactButton}>
+                    <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
                         <Image source={whatsappIcon}/>
                         <Text style={styles.contactButtonText}>Entrar em contato</Text>
                     </RectButton>

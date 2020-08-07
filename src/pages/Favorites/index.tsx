5c010589569482f16d1d5c8cbdf9a3d153deec45
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-08-07 13:03:28 -0300

    Finalizando controle de favoritos nos 2 canais

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import { ScrollView } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useFocusEffect } from '@react-navigation/native'
 
 import styles from './styles';
 
 function Favorites() {
-    return(
+    const [favorites, setFavorites] = useState([]);
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if (response) {
+
+                const favoritedTeachers = JSON.parse(response);
+                setFavorites(favoritedTeachers);
+            }
+        });
+    }
+
+    // Executa toda vez que a tela entrar em foco
+    // o useCallback evita que a aplicacao entre em loop
+    useFocusEffect(
+        React.useCallback(() => {
+          loadFavorites();
+        }, [])
+      )
+    
+    return (
         <View style={styles.container}>
             <PageHeader title="Meus proffys favoritos"/>
 
@@ -18,10 +40,15 @@ function Favorites() {
                   paddingBottom: 16
               }}
             >
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                { favorites.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem
+                            key={teacher.id}
+                            teacher={teacher}
+                            favorited={true}
+                        />
+                    );
+                }) }
             </ScrollView>
         </View>
     );

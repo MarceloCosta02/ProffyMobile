4a9179e7b4be2f752b2d44dddd164c2a9f2a544a
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-08-07 11:28:28 -0300

    Finalizando filtros de proffys

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import backIcon from '../../assets/images/icons/back.png';
@@ -9,9 +9,10 @@ import { useNavigation } from '@react-navigation/native';
 
 interface PageHeraderProps {
     title: string;
+    headerRight?: ReactNode; // Pode receber um componente como uma propriedade
 }
 
-const PageHeader: React.FC<PageHeraderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeraderProps> = ({ title, headerRight, children }) => {
     const { navigate } = useNavigation();
 
     function handleGoBack() {
@@ -28,7 +29,13 @@ const PageHeader: React.FC<PageHeraderProps> = ({ title }) => {
                 <Image source={logoImg} resizeMode="contain" />
             </View>
 
-            <Text style={styles.title}>{title}</Text>
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+
+                { headerRight }
+            </View>
+
+            {children}
         </View>
     );
 }
